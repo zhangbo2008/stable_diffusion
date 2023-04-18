9d7506c2f5bb4da8769b19af19ee42ee1a407ef3
zhangbo2008 <15122306087@163.com>
2023-04-18 15:29:52 +0800

    no_comment

--- train_dreambooth.py
+++ train_dreambooth.py
@@ -883,6 +883,7 @@ def main(args):
 if __name__ == "__main__":
     args = parse_args()
     args.pretrained_model_name_or_path='runwayml/stable-diffusion-v1-5'
+    args.pretrained_model_name_or_path='/stable-diffusion-v1-5'
     args.output_dir=OUTPUT_DIR
     args.seed=1337
     args.resolution=512
